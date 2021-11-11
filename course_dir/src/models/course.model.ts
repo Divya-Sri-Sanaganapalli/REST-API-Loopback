4362e91045f61cdfc061dc43af850200ef98b623
Divya-Sri-Sanaganapalli <dsanagan@uncc.edu>
2021-11-11 10:27:57 -0500

    REST API using Loopback

---
+++ course_dir/src/models/course.model.ts
@@ -0,0 +1,35 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Course extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  professor: string;
+
+
+  constructor(data?: Partial<Course>) {
+    super(data);
+  }
+}
+
+export interface CourseRelations {
+  // describe navigational properties here
+}
+
+export type CourseWithRelations = Course & CourseRelations;
